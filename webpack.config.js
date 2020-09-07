106d1c6339c387fa9dd3925685d171764c1d884e
yhuxuf <yhuxuf@gmail.com>
2020-09-07 23:15:22 +0100

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+       }
+};
+
